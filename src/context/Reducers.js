ed7df6b9f26ef06ed974d4f3693fa58cbfbf45ff
sergull <s.sopcali@gmail.com>
2023-11-03 14:33:33 +0300

    reducers commit

--- src/context/Reducers.js
+++ src/context/Reducers.js
@@ -1,42 +1,4 @@
-// export default function (state, action) {
-//     switch (action.type) {
-//         case "ADD_FAV":
 
-//             const fav = [...state.favList, action.payload.favorite]
-//             return { ...state, favList: fav }
-
-//         case 'DELETE_FAVORİTED':
-//             const mydata = action.payload.item;
-
-//             const newData = state.favList.filter(data => {
-//                 if (data.id !== mydata.id) return true;
-//             });
-//             return { ...state, JobsDetailId: newData };
-
-//         default:
-//             return state;
-//     }
-// }
-
-// export default function (state, action) {
-//     switch (action.type) {
-//         case "ADD_FAV":
-
-//             const fav = [...state.favList, action.payload.favorite]
-//             return { ...state, favList: fav }
-
-//         case 'DELETE_FAVORİTED':
-//             const mydata = action.payload.item;
-
-//             const newData = state.favList.filter(data => {
-//                 if (data.id !== mydata.id) return true;
-//             });
-//             return { ...state, JobsDetailId: newData };
-
-//         default:
-//             return state;
-//     }
-// }
 
 export default function (state, action) {
     switch (action.type) {
